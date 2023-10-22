d9acafa80e86d7ba2f38d926473d4402a4f79e96
Paul Adrian <uifalean.paul96@gmail.com>
2023-10-22 23:13:45 +0300

    Add project files.

---
+++ Data/Uifalean_Paul_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Uifalean_Paul_Lab2.Models;
+
+namespace Uifalean_Paul_Lab2.Data
+{
+    public class Uifalean_Paul_Lab2Context : DbContext
+    {
+        public Uifalean_Paul_Lab2Context (DbContextOptions<Uifalean_Paul_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Uifalean_Paul_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Uifalean_Paul_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
