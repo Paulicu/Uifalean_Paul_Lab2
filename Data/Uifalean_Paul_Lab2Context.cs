00bef19963f7c2083faa5126852359e97edb2959
Paul Adrian <uifalean.paul96@gmail.com>
2023-11-12 19:56:18 +0200

    Laborator 5

--- Data/Uifalean_Paul_Lab2Context.cs
+++ Data/Uifalean_Paul_Lab2Context.cs
@@ -21,5 +21,9 @@ namespace Uifalean_Paul_Lab2.Data
         public DbSet<Uifalean_Paul_Lab2.Models.Author>? Author { get; set; } = default!;
 
         public DbSet<Uifalean_Paul_Lab2.Models.Category>? Category { get; set; }
+
+        public DbSet<Uifalean_Paul_Lab2.Models.Member>? Member { get; set; }
+
+        public DbSet<Uifalean_Paul_Lab2.Models.Borrowing>? Borrowing { get; set; }
     }
 }
